446b5cdd67e82b4edfb3b3285df97b550fccd890
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-02-27 03:37:37 +0530

    Added auth middleware and token validator

---
+++ auth.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+)
+
+func TokenAuthMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var productId = r.URL.Query().Get("productId")
+		productInfo, ok := database[productId]
+		if !ok || productId == "" {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+		token := r.Header.Get("Authorization")
+		if !isValidToken(productInfo, token) {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+func isValidToken(productInfo ProductInfo, token string) bool {
+	if strings.HasPrefix(token, "Bearer ") {
+		return strings.TrimPrefix(token, "Bearer ") == productInfo.Token
+	}
+	return false
+}
