770e3cf26afe899aa126d6a90ed9219c4d569f48
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-02-27 02:47:15 +0530

    Added handlers for the web server with HTTP Verbs: GET, POST, PATCH

---
+++ handlers.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func handleProductInfo(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		GetProduct(w, r)
+	case http.MethodPost:
+		AddNewProduct(w, r)
+	case http.MethodPatch:
+		UpdateProductInfo(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func GetProduct(w http.ResponseWriter, r *http.Request) {
+	var productId = r.URL.Query().Get("productId")
+	productInfo, ok := database[productId]
+	if !ok || productId == "" {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	response := ProductInfo{
+		Id:          productId,
+		Name:        productInfo.Name,
+		Price:       productInfo.Price,
+		Description: productInfo.Description,
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func AddNewProduct(w http.ResponseWriter, r *http.Request) {
+	var productId = r.URL.Query().Get("productId")
+	// Decode the JSON payload directly into the struct
+	var payloadData ProductInfo
+	if err := json.NewDecoder(r.Body).Decode(&payloadData); err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	database[productId] = payloadData
+	w.WriteHeader(http.StatusCreated)
+}
+
+func UpdateProductInfo(w http.ResponseWriter, r *http.Request) {
+	var productId = r.URL.Query().Get("productId")
+	log.Println("Received Product ID:", productId)
+	productInfo, ok := database[productId]
+	if !ok || productId == "" {
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+	// Decode the JSON payload directly into the struct
+	var payloadData ProductInfo
+	if err := json.NewDecoder(r.Body).Decode(&payloadData); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	// Update the product information
+	log.Println("Decoded JSON payload data is:", payloadData)
+	productInfo.Id = productId
+	productInfo.Name = payloadData.Name
+	productInfo.Price = payloadData.Price
+	productInfo.Description = payloadData.Description
+	database[productInfo.Id] = productInfo
+	w.WriteHeader(http.StatusOK)
+}
