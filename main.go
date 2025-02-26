a4fce503b1c5972ebaeb6ae5695574f194fbed9a
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2025-02-27 03:41:46 +0530

    Added middleware wrapper on handler function for product APIs

--- main.go
+++ main.go
@@ -5,8 +5,20 @@ import (
 	"net/http"
 )
 
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+var middlewares = []Middleware{
+	TokenAuthMiddleware,
+}
+
 func main() {
-	http.HandleFunc("/product/info", handleProductInfo)
+	var handler http.HandlerFunc = handleProductInfo
+
+	for _, middleware := range middlewares {
+		handler = middleware(handler)
+	}
+
+	http.HandleFunc("/product/info", handler)
 
 	log.Println("Server is running on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
